8eb98ac961e177baad33efe622265b9e60c9b100
BT-emahgoub <eslam.mahgoub@braintec.com>
2025-05-09 03:39:11 +0200

    Add Axios and get weather api data

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,8 +6,55 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Box, Container, Divider, Grid } from "@mui/material";
 import CloudIcon from "@mui/icons-material/Cloud";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function WeatherCard() {
+  const [temp, setTemp] = useState({
+    number: null,
+    description: "",
+    min: null,
+    max: null,
+    icon: null,
+  });
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=50.04&lon=19.94&appid=d97daca08149d36f95a0388af8c69642",
+        {
+          signal: controller.signal,
+        }
+      )
+      .then(function (response) {
+        const responseTemp = Math.round(response.data.main.temp - 272.15);
+        const min = Math.round(response.data.main.temp_min - 272.15);
+        const max = Math.round(response.data.main.temp_max - 272.15);
+        const description = response.data.weather[0].description;
+        const responseIcon = response.data.weather[0].icon;
+        setTemp({
+          number: responseTemp,
+          min,
+          max,
+          description,
+          icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+        });
+      })
+      .catch(function (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled:", error.message);
+        } else {
+          console.log("Error:", error);
+        }
+      })
+      .finally(function () {}, []);
+
+    return () => {
+      controller.abort("Component unmounted");
+    };
+  }, []);
+
   return (
     <Container maxWidth="md">
       <Card
@@ -41,13 +88,17 @@ export default function WeatherCard() {
               size={8}
               style={{ marginTop: "20px", mt: 2, textAlign: "right" }}
             >
-              <Typography variant="h1">38</Typography>
-              {/* Todo: weather image */}
-              <Typography variant="h6">broken clouds</Typography>
               <Box sx={{ display: "flex" }}>
-                <Typography variant="h5">الكبري:28</Typography>
+                <Typography variant="h1">{temp.number}</Typography>
+                <img src={temp.icon} alt="Temperature Icon" />
+              </Box>
+
+              {/* Todo: weather image */}
+              <Typography variant="h6">{temp.description}</Typography>
+              <Box sx={{ display: "flex" }}>
+                <Typography variant="h5">الصغري: {temp.min} </Typography>
                 <Typography variant="h5">|</Typography>
-                <Typography variant="h5">الصغري:15</Typography>
+                <Typography variant="h5">الكبري: {temp.max}</Typography>
               </Box>
             </Grid>
             <Grid size={4}>
